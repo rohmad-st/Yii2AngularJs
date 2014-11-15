4e1e06e4ccd6006c89ea5ac22e12b2c371bb4fd7
Edi Santoso <edicyber@gmail.com>
2014-11-15 22:22:10 +0700

    fix db name

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class'    => 'yii\db\Connection',
-    'dsn'      => 'mysql:host=localhost;dbname=appdesa',
+    'dsn'      => 'mysql:host=localhost;dbname=app_school',
     'username' => 'root',
     'password' => 'root',
     'charset'  => 'utf8',
