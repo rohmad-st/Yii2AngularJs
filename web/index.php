49ce5d087cf0d5b67df26ce83431b4e56551670c
Edi Santoso <edicyber@gmail.com>
2015-01-10 20:12:17 +0700

    register the application service

--- web/index.php
+++ web/index.php
@@ -8,8 +8,17 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-// Register the provider
-\Yii::$container->set('app\SchoolApp\Repositories\StudentInterface', 'app\SchoolApp\Repositories\ActiveRecord\StudentRepository');
+/*
+* Register The Application Services
+*/
+
+// Register The Employee Service
+\Yii::$container->set('app\EmpApp\Repositories\Employee\EmployeeInterface',
+    'app\EmpApp\Repositories\Employee\EmployeeRepository');
+
+// Register The Department Service
+\Yii::$container->set('app\EmpApp\Repositories\Department\DepartmentInterface',
+    'app\EmpApp\Repositories\Department\DepartmentRepository');
 
 $config = require(__DIR__ . '/../config/web.php');
 
